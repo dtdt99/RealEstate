70082fe5bf156b9efa177bc7e46c4aa4a88c8f56
Dodgson Tan <dtan13409@gmail.com>
2022-06-11 16:17:33 +1200

    first commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -9,7 +9,7 @@ export const fetchApi = async (url) => {
     const {data} = await axios.get((url),{
         headers: {
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-            'X-RapidAPI-Key': 'bdde72a415mshbfd5dc69c450720p10f3f4jsnec1542010c22'
+            'X-RapidAPI-Key': process.env.API_KEY
           }
     });
     return data;
